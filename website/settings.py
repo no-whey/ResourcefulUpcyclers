9e92f40b4d74f4c3c7743b95f4ae0adbf48546a4
Ryan Schouweiler <rybluemip@gmail.com>
2018-03-10 19:06:51 -0800

    added STATIC_ROOT

--- website/settings.py
+++ website/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/frontend/static/'
 STATICFILES_DIRS = (
     PROJECT_DIR.child('frontend').child('static'),
