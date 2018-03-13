ec5db7327ca0895180df9a3c0c386aa59665ed57
Ryan Schouweiler <rybluemip@gmail.com>
2018-03-12 17:51:55 -0700

    Removed unneccessary SECRET_KEY

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ COURSE_DIR = Path(__file__).parent
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
