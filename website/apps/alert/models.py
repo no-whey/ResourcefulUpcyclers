78a38b61f611bc39b4f17f88339b3259516b2ab8
reschouw <rybluemip@gmail.com>
2018-04-16 17:07:19 -0700

    Added first iteration of alert model and registered on admin interface for testing

--- website/apps/alert/models.py
+++ website/apps/alert/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from model_utils import Choices
+from model_utils.fields import StatusField
+
+
+# Generic alert model
+class Alert(models.Model):
+    STATUS_OPTIONS = Choices('unread', 'read')
+    
+    status = StatusField(choices_name='STATUS_OPTIONS', default='unread')
+    message = models.TextField(max_length = 200, blank = True, default='', null=True)
+    user = models.ForeignKey(User, related_name='alert_user', on_delete=models.CASCADE)
+    
