b693cb6f2d8b952d5d483f2d8f69eb13574d65e5
reschouw <rybluemip@gmail.com>
2018-04-21 19:58:50 -0700

    ALERTS NOW SHOW UP! HALLELUJAH

--- website/apps/alert/models.py
+++ website/apps/alert/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 from model_utils import Choices
 from model_utils.fields import StatusField
+from django.contrib.auth.models import User
+
+from website.apps.profiles.models import Profile
 
 
 # Generic alert model
@@ -9,6 +11,7 @@ class Alert(models.Model):
     STATUS_OPTIONS = Choices('unread', 'read')
     
     status = StatusField(choices_name='STATUS_OPTIONS', default='unread')
+    #title = models.TextField(max_length = 200, blank = True, default='', null=True)
     message = models.TextField(max_length = 200, blank = True, default='', null=True)
-    user = models.ForeignKey(User, related_name='alert_user', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
