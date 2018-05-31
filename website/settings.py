63af3c1ede84802b21433e274f873485695fdf54
Ryan Schouweiler <rybluemip@gmail.com>
2018-05-30 21:02:34 -0700

    Trying edit to STATIC_ROOT

--- website/settings.py
+++ website/settings.py
@@ -143,11 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    PROJECT_DIR.child('frontend').child('static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
