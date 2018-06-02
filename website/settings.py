dcd43a249c371a1c4453b4c6373d03a9c08a8bb2
Ryan Schouweiler <rybluemip@gmail.com>
2018-06-02 12:04:18 -0700

    testing additional static files settings

--- website/settings.py
+++ website/settings.py
@@ -145,6 +145,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
