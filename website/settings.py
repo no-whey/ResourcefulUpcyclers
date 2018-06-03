55acb6d0ac3f048605bc46a0fdf5a3cc07294247
Ryan Schouweiler <rybluemip@gmail.com>
2018-06-03 11:15:01 -0700

    Resetting Static url to test

--- website/settings.py
+++ website/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
